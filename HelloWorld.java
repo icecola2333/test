8a9ac5924f35f71e0ffedc70bdcdf6f0d3ff3692
icecola2333 <68260591+icecola2333@users.noreply.github.com>
2020-07-14 11:48:23 +0800

    测试编辑文件的功能
    第一次修改

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 public class HelloWorld {
    public static void main(String[] args) {
-  System.out.println('HelloWorld');
+      System.out.println('HelloWorld1');
+      System.out.println('HelloWorld2');
 }
